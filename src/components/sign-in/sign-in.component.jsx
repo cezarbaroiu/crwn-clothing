7886ae56d9b239ce1b532553041c6b063ed0e7a9
cezar.baroiu <cezar.baroiu@gmail.com>
2020-08-11 17:26:54 +0300

    Add custom button and style for it, replace input with custom button in SignIn component

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
+
+import './sign-in.styles.scss';
+
+class SignIn extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+    }
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    this.setState({ email: '', password: '' });
+  }
+
+  handleChange = event => {
+    const { name, value } = event.target;
+
+    this.setState({ [name]: value });
+  }
+
+  render() {
+    const { email, password } = this.state;
+
+    return (
+      <div className="sign-in">
+        <h2>I already have an account</h2>
+        <span>Sign in with your email and password</span>
+
+        <form onSubmit={this.handleSubmit}>
+          <FormInput 
+            name="email"
+            type="email"
+            value={email}
+            handleChange={this.handleChange}
+            label="email"
+            required
+          />
+          <FormInput 
+            name="password"
+            type="password"
+            value={password}
+            handleChange={this.handleChange}
+            label="password"
+            required
+          />
+
+          <CustomButton type="submit">
+            Sign in
+          </CustomButton>
+        </form>
+      </div>
+    )
+  }
+};
+
+export default SignIn;
