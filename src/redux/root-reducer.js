8fe1ebe747e385246e2cd33cc55bcf4a4c3e14dd
cezar.baroiu <cezar.baroiu@gmail.com>
2020-11-16 21:25:25 +0200

    Normalize shop date from array to an object Add selector in shop selector - selectCollectionsForPreview

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,26 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
+import directoryReducer from './directory/directory.reducer';
+import shopReducer from './shop/shop.reducer';
 
-export default combineReducers({
+// configuration object for the reducer persist
+// we start the persisting of the reducers from the root
+const persistConfig = {
+  key: 'root',
+  storage: storage,
+  whitelist: ['cart']
+}
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
-})   
+  directory: directoryReducer,
+  shop: shopReducer,
+});
+
+// we return the root reducer with the persist capabilities on top of it
+export default persistReducer(persistConfig, rootReducer);
---
+++ src/redux/shop/shop.selectors.js
@@ -0,0 +1,18 @@
+import { createSelector } from 'reselect';
+
+const selectShop = state => state.shop;
+
+export const selectCollections = createSelector (
+  [selectShop],
+  (shop) => shop.collections //this function returns the collection "promissed" by the selectCollections
+);
+
+export const selectCollectionsForPreview = createSelector (
+  [selectCollections],
+  collections => Object.keys(collections).map(key => collections[key])
+)
+
+export const selectCollection = (collectionUrlParam) => createSelector(
+  [selectCollections],
+  collections => collections[collectionUrlParam]
+); 
